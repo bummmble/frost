95dc05bd5c3e4d062ac6355284f85334bbe62808
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-02 22:54:27 -0500

    Added utility function

---
+++ packages/frost-builder/src/utils.js
@@ -0,0 +1,9 @@
+export const removeEmptyKeys = obj => {
+	const res = {};
+	for (const key in obj) {
+		if (!(obj[key] === null || obj[key].length === 0)) {
+			res[key] = obj[key];
+		}
+	}
+	return res;
+};
