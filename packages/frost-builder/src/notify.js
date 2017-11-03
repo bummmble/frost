c9c7ef293c1b05914ebc04c9bbbce4a16b8da279
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-03 15:46:44 -0500

    Adds a notify plugin to be optionally used

---
+++ packages/frost-builder/src/notify.js
@@ -0,0 +1,27 @@
+import notifier from 'node-notifier';
+import Logger from './helpers/console';
+
+export default options => {
+	const title = `${options.title}`;
+	if (options.notify) {
+		notifier.notify({
+			title,
+			message: options.message
+		});
+	}
+
+	const level = options.level || 'info';
+	const message = `${chalk.bold(title)}: ${options.message}`;
+
+	switch (level) {
+		case 'warn':
+			console.log(Logger.warning(message));
+			break;
+		case 'error':
+			console.log(Logger.error(message));
+			break;
+		case 'info':
+		default:
+			console.log(Logger.info(message));
+	}
+};
