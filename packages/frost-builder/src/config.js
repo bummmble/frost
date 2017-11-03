226b70b4350560d61d9bd20e2ba59ebb3741816c
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-02 23:16:55 -0500

    Added a simple config loader with cosmiconfig

---
+++ packages/frost-builder/src/config.js
@@ -0,0 +1,52 @@
+import cosmiconfig from 'cosmiconfig';
+import { get as getRoot } from 'app-root-dir';
+import { get, set, defaultsDeep } from 'lodash';
+import { relative, resolve, join } from 'path';
+import chalk from 'chalk';
+import defaults from './defaults';
+
+const Root = getRoot();
+const resolveFor = [
+	'entry.client',
+	'entry.server',
+	'entry.clientVendor',
+	'output.server',
+	'output.client'
+];
+
+const configLoader = cosmiconfig('frost', {
+	stopDir: Root
+});
+
+const configPromise = configLoader
+	.load(Root)
+	.then(results => {
+		const merged = defaultsDeep(results.config, defaults);
+		return resolvePaths(merged);
+	})
+	.catch(error => {
+		throw new Error(`Error parsing frost-config file: ${error}`);
+	})
+
+const getConfig = async flags => {
+	return await configPromise
+		.then(config => {
+			for (const key in flags) {
+				set(config, key, flags[key]);
+			}
+			return config;
+		})
+};
+
+const resolvePaths = config => {
+	resolveFor.forEach(loc => {
+		if (get(config, loc) != null) {
+			set(config, entry, resolve(Root, get(config, loc)));
+		}
+	});
+	return config;
+};
+
+export {
+	getConfig
+}
