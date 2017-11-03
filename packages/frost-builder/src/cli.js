57fe499d13cd94959b48355df48febaa9a4c8786
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-03 01:14:24 -0500

    Added small cli interface

---
+++ packages/frost-builder/src/cli.js
@@ -0,0 +1,41 @@
+import meow from 'meow';
+import chalk from 'chalk';
+import { getConfig } from './config';
+import { buildClient } from './commands/build';
+
+const cli = meow(`
+	Usage:
+		$ frost <command>
+
+	Commands:
+		build:client        Builds a production version of the client
+`);
+
+const input = cli.input;
+const flags = cli.flags;
+const tasks = [
+	{ task: 'build:client', commands: [buildClient] }
+];
+
+async function executeTasks() {
+	const config = await getConfig(flags);
+	for (const name of input) {
+		for (const task of tasks) {
+			if (task.task === name) {
+				try {
+					// execute tasks
+				} catch (error) {
+					console.error(chalk.bold.red(`Failed to execute task ${name}. Error ${error}`));
+					console.error(error);
+					process.exit(1);
+				}
+			}
+		}
+	}
+}
+
+if (input.length > 0) {
+	process.nextTick(executeTasks);
+} else {
+	command.showHelp();
+}
