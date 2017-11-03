30f626c572192293254d11d0f82aa31b0e2a66cc
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-03 15:48:06 -0500

    Exposes an index file

---
+++ packages/frost-builder/src/index.js
@@ -0,0 +1,3 @@
+export { buildClient, buildServer, cleanServer, cleanClient } from './commands/build';
+export { default as compiler } from './compiler';
+export { default as notify } from './notify';
