0fb77e764c587f91681b91264b2a8fddd29568f3
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-02 22:48:37 -0500

    Added a default configuration file

---
+++ packages/frost-builder/src/defaults.js
@@ -0,0 +1,29 @@
+export default {
+	entry: {
+		client: 'client/index.js',
+		server: 'server/index.js'
+	},
+
+	output: {
+		client: 'build/client',
+		server: 'build/server',
+		public: '/static/'
+	},
+
+	files: {
+		babel: /\.(js|mjs|jsx)$/,
+		styles: /\.(css|sss|pcss)$/,
+		images: /\.(jpg|png|gif)$/,
+		fonts: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+	},
+
+	cacheLoader: {},
+	postcss: true,
+	sourceMaps: true,
+	compression: {},
+	babel: {},
+	prettier: {},
+	eslint: {},
+	performance: {},
+	pwa: {}
+}
