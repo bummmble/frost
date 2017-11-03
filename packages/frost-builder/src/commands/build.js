f57153866adb65ffc02beb972b2cd77c7241afde
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-03 01:22:20 -0500

    Much better formatting for the build command

--- packages/frost-builder/src/commands/build.js
+++ packages/frost-builder/src/commands/build.js
@@ -3,30 +3,13 @@ import chalk from 'chalk';
 import { remove } from 'fs-extra';
 
 import compiler from '../compiler';
-import { formatWebpack } from '../format/webpack';
+import formatOutput from '../format/output';
 
 const buildClient = (config = {}) => {
 	const webpackConfig = compiler('client', 'production', config);
 	return new Promise((resolve, reject) => {
 		webpack(webpackConfig, (error, stats) => {
-			const raw = stats.toJson();
-			const { errors, warnings } = formatWebpack(raw);
-
-			if (error) {
-				const msg = `Fatal error while compiling client. Error: ${error}`;
-				console.log(chalk.red(msg));
-				return reject(msg);
-			}
-
-			if (!errors.length && !warnings.length) {
-				console.log(chalk.green('Client build success!'));
-			}
-
-			if (errors.length) {
-				return reject(errors.join('\n\n'));
-			}
-
-			return resolve(true);
+			return resolve(formatOutput(error, stats));
 		});
 	});
 };
---
+++ packages/frost-builder/src/format/output.js
@@ -0,0 +1,31 @@
+import chalk from 'chalk';
+import { formatWebpack } from './webpack';
+
+export default (error, stats, target) => {
+	if (error) {
+		const msg = `Fatal error while compiling ${target}. Error: ${error}`;
+		console.log(chalk.red(msg));
+		return Promise.reject(msg);
+	}
+
+	const raw = stats.toJson({});
+	const { errors, warnings } = formatWebpack(raw);
+	
+	const isSuccessful = !errors.length && !warnings.length;
+	if (isSuccessful) {
+		console.log(chalk.green(`Compiled ${target} successfully`));
+	}
+
+	if (errors.length) {
+		console.log(chalk.red(`Failed to compile ${target}`));
+		console.log(messages.errors.join('\n\n'));
+		return Promise.reject(`Failed to compile ${target}`);
+	}
+
+	if (warnings.length && !errors.length) {
+		console.log(chalk.yellow(`Compiled ${target} with warnings`));
+		console.log(warnings.join('\n\n'));
+	}
+
+	return Promise.resolve(true);
+}
