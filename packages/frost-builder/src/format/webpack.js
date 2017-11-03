2af858b81d962c143749b66b63785a74e5c6bb3f
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-03 01:03:47 -0500

    Added a formatting script to clean up webpack messages a bit

---
+++ packages/frost-builder/src/format/webpack.js
@@ -0,0 +1,29 @@
+const formatRaw = (message, isError) => {
+	let lines = message.split('\n');
+
+	if (lines.length > 2 && lines[1] === '') {
+		lines.splice(1, 1);
+	}
+	if (lines[0].lastIndexOf('!') !== -1) {
+		lines[0] = lines[0].substr(lines[0].lastIndexOf('!') + 1);
+	}
+
+	lines = lines.filter(line => lines.indexOf(' @ ') !== 0);
+	if (!lines[0] || !lines[1]) {
+		return lines.join('\n');
+	}
+
+	message = lines.join('\n');
+	return message.trim();
+};
+
+const formatWebpack = json => {
+	const errors = json.errors.map(message => formatWebpack(message, true));
+	const warnings = json.warnings.map(message => formatWebpack(message, false));
+	return { errors, warnings };
+};
+
+export {
+	formatRaw,
+	formatWebpack
+}
