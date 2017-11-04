c89779c4af1579b894755240499cf0df5f726276
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-04 16:09:26 -0500

    Added some more middleware

---
+++ packages/frost-server/src/express/error.js
@@ -0,0 +1,13 @@
+import PrettyError from 'pretty-error';
+
+const pretty = new PrettyError();
+
+pretty.skipNodeFiles();
+pretty.skipPackage('express');
+
+export default server => {
+	server.use((err, req, res, next) => {
+		console.log(pretty.render(err));
+		next();
+	});
+};
