677552d04647d67eacc159fa40af233da4441119
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-04 18:15:24 -0500

    Adds memoize string method

--- packages/frost-utils/src/strings.js
+++ packages/frost-utils/src/strings.js
@@ -10,3 +10,13 @@ export const camelize = string => {
 export const hyphenate = string => {
 	return string.replace(uppercasePattern, '-$1').toLowerCase();
 };
+
+export const memoizeString = cb => {
+	const cache = {};
+	return string => {
+		if (!cache.hasOwnProperty(string)) {
+			cache[string] = cb.call(this, string);
+		}
+		return cache[string];
+	}
+}
