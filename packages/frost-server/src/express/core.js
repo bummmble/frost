9c1cbc0174a251eac93a907359243962d2c424d5
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-04 16:08:12 -0500

    Started working on a server util

---
+++ packages/frost-server/src/express/core.js
@@ -0,0 +1,8 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+
+export default server => {
+	server.use(cookieParser());
+	server.use(express.json());
+	server.use(express.urlencoded({ extended: true }));
+};
