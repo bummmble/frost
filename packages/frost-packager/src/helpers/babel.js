1f180a1ea2109c6afb0bdd852ef6a3a848e334ef
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-04 19:37:34 -0500

    Added transpiler options for the packager

---
+++ packages/frost-packager/src/helpers/babel.js
@@ -0,0 +1,10 @@
+import babel from 'rollup-plugin-babel';
+export default () => ({
+	classic: babel({
+		runtimeHelpers: true,
+		exclude: [
+			'node_modules/**',
+			'**/*.json'
+		]
+	})
+});
---
+++ packages/frost-packager/src/helpers/buble.js
@@ -0,0 +1,5 @@
+import buble from 'rollup-plugin-buble';
+
+export default () => {
+	return buble();
+};
