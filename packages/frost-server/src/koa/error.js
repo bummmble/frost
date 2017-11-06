79d7c9808cdcee4bf7206b182a53672571547a6b
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-05 18:30:49 -0600

    Adds some middleware to koa

---
+++ packages/frost-server/src/koa/error.js
@@ -0,0 +1,5 @@
+import errorHandler from 'koa-better-error-handler';
+
+export default server => {
+	server.context.onerror = errorHandler;
+};
---
+++ packages/frost-server/src/koa/fallback.js
@@ -0,0 +1,5 @@
+import koa404 from 'koa-404-handler';
+
+export default server => {
+	server.use(koa404());
+};
