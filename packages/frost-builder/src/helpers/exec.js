61ccdcbbc32261dbffece05e57981ecab05f1a05
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-05 18:16:21 -0600

    Adds exec helper

---
+++ packages/frost-builder/src/helpers/exec.js
@@ -0,0 +1,12 @@
+import { execFileSync } from 'child_process';
+
+export const exec = (command, args) => {
+	const options = {
+		cwd: process.cwd(),
+		env: process.env,
+		stdio: 'pipe',
+		encoding: 'utf-8'
+	};
+
+	return execFileSync(command, args, options);
+};
