3bda0aa0208c3fbee3eea643eba7f4b6e93f64c0
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-07 12:38:09 -0600

    Adds some comments for clairity

--- packages/frost-react-core/src/server/import.js
+++ packages/frost-react-core/src/server/import.js
@@ -1,5 +1,7 @@
 import { CHUNK_NAMES } from 'react-universal-component';
 
+// Sync loads the given module on the server by acting on
+// Transpiled import() statements from babel-plugin-universal-import
 export const loadImport = wrapped => {
 	const module = __webpack_require__(wrapped.resolve());
 	return module && module.__esModule
@@ -7,6 +9,9 @@ export const loadImport = wrapped => {
 		: module;
 };
 
+
+// Register the module for pre-loading on the client
+// This injects the chunk name for flushing to the generated HTML
 export const preloadImport = wrapped => {
 	CHUNK_NAMES.add(wrapped.chunkName());
 };
