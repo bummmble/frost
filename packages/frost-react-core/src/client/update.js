359f75491a1bb2496be91efde4b48fbd554d9626
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-07 18:31:00 -0600

    Adds an update helper function

---
+++ packages/frost-react-core/src/client/update.js
@@ -0,0 +1,8 @@
+import { createRootReducer } from '../shared/state';
+
+export default function update(next, internals) {
+	internals.store.replaceReducer(createRootReducer(
+		next.getReducers(),
+		internals.router
+	));
+}
