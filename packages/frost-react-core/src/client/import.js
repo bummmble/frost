8c765b2ea76af4209d4d07af78ef1dbaec64a414
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-07 18:28:59 -0600

    adds client import

---
+++ packages/frost-react-core/src/client/import.js
@@ -0,0 +1,11 @@
+export function loadImport(wrapped) {
+	return wrapped.then(module => {
+		return module && module.__esModule
+			? module.default
+			: module
+	});
+}
+
+export function preloadImport(wrapped) {
+	return wrapped.load();
+}
