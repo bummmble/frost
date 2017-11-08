eeee84dcc9b1ee531ded33ce44f40cd0c551d0a8
Forrest Collins <Forest.D.Collins@gmail.com>
2017-11-07 18:39:40 -0600

    Adds more array tests

--- packages/frost-utils/tests/array.js
+++ packages/frost-utils/tests/array.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { compactArray, concatAllArray } from '../src/array';
+import { compactArray, concatAllArray, flatMapArray, groupArray } from '../src/array';
 
 test('Compact array filters null and undefined', t => {
   const original = [1, 2, 3, null, 4, undefined, 5];
@@ -14,3 +14,18 @@ test('ConcatAll concats an array of arrays', t => {
 test('ConcatAll ignores null and undefined', t => {
   t.is(concatAllArray([null, undefined, [1], [2]], x => x), [1, 2]);
 });
+
+test('Flat map should flatten a deep array', t => {
+	t.is(
+		flatMapArray([1, 2, 3], x => [x, x + 1]),
+		[1, 2, 2, 3, 3, 4]
+	);
+});
+
+test('Flat map should ignore null and undefined', t => {
+	t.is(
+		flatMapArray([null, undefined, [1], [2]], x => x),
+		[1, 2]
+	);
+})
+
