6b9d07240d27dcbf0b3f1e308fa8bb94ffb41c5e
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-13 13:04:44 -0600

    Adds more eslint rules

--- packages/frost-eslint/src/index.js
+++ packages/frost-eslint/src/index.js
@@ -6,6 +6,7 @@ module.exports = {
     './rules/modern.yml',
     './rules/node.yml',
     './rules/react.yml',
-    './rules/formatting.yml'
+    './rules/formatting.yml',
+    './rules/promise.yml'
   ],
 };
