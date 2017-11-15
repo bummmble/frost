6b6c243237653ee23d16d7bffc3a830ca796c919
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-15 11:54:57 -0600

    Finishing restructuring array utils

---
+++ packages/frost-utils/src/array/flatMapArray.js
@@ -0,0 +1,10 @@
+export function flatMapArray(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const res = fn.call(arr, arr[i], i);
+        if (Array.isArray(res)) {
+            result.push(res);
+        }
+    }
+    return result;
+}
---
+++ packages/frost-utils/src/array/groupArray.js
@@ -0,0 +1,11 @@
+export function groupArray(arr, fn) {
+    const result = {};
+    for (let i = 0; i < arr.length; i++) {
+        const res = fn.call(arr, arr[i], i);
+        if (!result[res]) {
+            result[res] = [];
+        }
+        result[res].push(arr[i]);
+    }
+    return res;
+}
