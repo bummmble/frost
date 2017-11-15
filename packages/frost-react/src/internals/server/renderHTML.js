b305c1b9430b5cf198f33603dfae05a15ed56d46
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-15 16:33:50 -0600

    renders html

---
+++ packages/frost-react/src/internals/server/renderHTML.js
@@ -0,0 +1,33 @@
+import serialize from 'serialize-javascript';
+import Helmet from 'react-helmet';
+
+export default function renderHTML({
+    state,
+    html,
+    styles,
+    scripts
+}) {
+    const frost = state.frost;
+    const helmet = Helmet.renderStatic();
+    const inlineCode = `APP_STATE=${serialize(state, { isJSON: true })}`;
+    const nonceHtml = frost.nonce ? `nonce="${frost.nonce}"` : '';
+
+    return `
+    <!doctype html>
+    <html ${helmet.htmlAttributes.toString()}>
+        <head>
+            ${helmet.title.toString()}
+            ${helmet.meta.toString()}
+            ${helmet.link.toString()}
+            ${styles}
+            ${helmet.styles.toString()}
+        </head>
+        <body>
+            <div id="root">${html}</div>
+            <script ${nonceHtml}>${inlineCode}</script>
+            ${scripts}
+            ${helmet.script.toString()}
+        </body>
+    </html>
+    `;
+}
