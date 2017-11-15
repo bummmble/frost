962b5ab1292e208d4e3bc441ac4195bdff7f7c9d
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-15 12:18:07 -0600

    Adds promiseMap based on promiseDeferred

---
+++ packages/frost-utils/src/promise/promiseMap.js
@@ -0,0 +1,28 @@
+import { Deferred } from './promiseDeferred';
+
+class PromiseMap {
+    constructor() {
+        this._deferred = {};
+    }
+
+    get(key) {
+        return getDeferred(this._deferred, key).getPromise();
+    }
+
+    resolveKey(key, value) {
+        const entry = getDeferred(this, deferred, key);
+        entry.resolve(value);
+    }
+
+    rejectKey(key, reason) {
+        const entry = getDeferred(this._deferred, key);
+        entry.reject(reason);
+    }
+}
+
+function getDeferred(entries, key) {
+    if (!entries.hasOwnProperty(key)) {
+        entries[key] = new Deferred();
+    }
+    return entries[key];
+}
