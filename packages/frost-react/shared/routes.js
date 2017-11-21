59fc3ef0d215e326db4ce60a56aa406ee4b7d74c
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-20 18:43:02 -0600

    Creates the shared store config

---
+++ packages/frost-react/shared/routes.js
@@ -0,0 +1,5 @@
+import { NOT_FOUND } from 'redux-first-router';
+
+export default {
+    HOME: '/'
+}
--- packages/frost-react/shared/store.js
+++ packages/frost-react/shared/store.js
@@ -2,8 +2,34 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import { connectRoutes } from 'redux-first-router';
 
+import routes from './routes';
+import * as reducers from './state/reducers/index';
+
 const composeEnhancers = (...args) => {
     return typeof window !== 'undefined'
         ? composeWithDevTools({})(...args)
         : compose(...args)
 };
+
+export default function configureStore(history, preloadedState) {
+    const { reducer, middleware, enhancer, thunk } = connectRoutes(
+        history,
+        routes
+    );
+
+    const rootReducer = combineReducers({ ...reducers, location: reducer });
+    const middlewares = applyMiddleware(middleware);
+    const enhancers = composeEnhancers(enhancer, middlewares);
+    const store = createStore(rootReducer, preloadedState, enhancers);
+
+    if (module.hot && process.env.NODE_ENV === 'development') {
+        module.hot.accept('./state/reducers/index', () => {
+            const reducers = require('./state/reducers/index');
+            const rootReducer = combineReducers({ ...reducers, location: renducer });
+            store.replaceReducer(rootReducer);
+        });
+    }
+
+    return { store, thunk };
+}
+
