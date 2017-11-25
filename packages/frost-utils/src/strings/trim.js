2d335204164d4c5116955b0ba6030828bd00ed31
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-25 10:07:28 -0600

    Release frost-utils@.3.2

--- packages/frost-utils/src/strings/trim.js
+++ packages/frost-utils/src/strings/trim.js
@@ -1,10 +1,10 @@
-import { defaultWhitespace } from './helpers'
+import { defaultWhiteSpace } from './helpers'
 
 export function trim(str, chars) {
   if (String.prototype.trim && !chars) {
     return String.prototype.trim.call(str)
   }
 
-  chars = defaultWhitespace(chars)
+  chars = defaultWhiteSpace(chars)
   return str.replace(new RegExp(`^${chars}+|${chars}|$`, 'g'), '')
 }
