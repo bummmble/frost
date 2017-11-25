c4181636f5fe96e64216a6e096d637b46bea433f
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-25 10:04:55 -0600

    Adds new string methods to utils index

--- packages/frost-utils/src/index.js
+++ packages/frost-utils/src/index.js
@@ -1,43 +1,49 @@
-export { compactArray } from './array/compactArray';
-export { concatAllArray } from './array/concatAllArray';
-export { flatMapArray } from './array/flatMapArray';
-export { flattenArray } from './array/flattenArray';
-export { groupArray } from './array/groupArray';
-export { partitionArray } from './array/partitionArray';
-export { uniqueArrayElements } from './array/uniqueArrayElements';
+export { compactArray } from './array/compactArray'
+export { concatAllArray } from './array/concatAllArray'
+export { flatMapArray } from './array/flatMapArray'
+export { flattenArray } from './array/flattenArray'
+export { groupArray } from './array/groupArray'
+export { partitionArray } from './array/partitionArray'
+export { uniqueArrayElements } from './array/uniqueArrayElements'
 
-export { emptyObject } from './objects/emptyObject';
-export { objectEvery } from './objects/objectEvery';
-export { objectFilter } from './objects/objectFilter';
-export { objectForEach } from './objects/objectForEach';
-export { objectIs } from './objects/objectIs';
-export { objectMap } from './objects/objectMap';
-export { objectRemoveEmpty } from './objects/objectRemoveEmpty';
-export { objectSome } from './objects/objectSome';
-export { partitionObject } from './objects/partitionObject';
-export { partitionObjectByKeys } from './objects/partitionObjectByKeys';
-export { shallowEqual } from './objects/shallowEqual';
+export { emptyObject } from './objects/emptyObject'
+export { objectEvery } from './objects/objectEvery'
+export { objectFilter } from './objects/objectFilter'
+export { objectForEach } from './objects/objectForEach'
+export { objectIs } from './objects/objectIs'
+export { objectMap } from './objects/objectMap'
+export { objectRemoveEmpty } from './objects/objectRemoveEmpty'
+export { objectSome } from './objects/objectSome'
+export { partitionObject } from './objects/partitionObject'
+export { partitionObjectByKeys } from './objects/partitionObjectByKeys'
+export { shallowEqual } from './objects/shallowEqual'
 
-export { camelize } from './strings/camelize';
-export { hyphenate } from './strings/hyphenate';
-export { memoizeString } from './strings/memoizeString';
+export { camelize } from './strings/camelize'
+export { capitalize } from './strings/capitalize'
+export { humanize } from './strings/humanize'
+export { hyphenate } from './strings/hyphenate'
+export { memoizeString } from './strings/memoizeString'
+export { trim } from './strings/trim'
+export { truncate } from './strings/truncate'
+export { underscored } from './strings/underscored'
+export { wrap } from './strings/wrap'
 
-export { default as Deferred } from './promise/promiseDeferred';
-export { default as PromiseMap } from './promise/promiseMap';
-export { promisify } from './promise/promisify';
-export { each } from './promise/promiseEach';
+export { default as Deferred } from './promise/promiseDeferred'
+export { default as PromiseMap } from './promise/promiseMap'
+export { promisify } from './promise/promisify'
+export { each } from './promise/promiseEach'
 
-export { default as areEqual } from './types/areEqual';
-export { isArray } from './types/isArray';
-export { isBoolean } from './types/isBoolean';
-export { isEmpty } from './types/isEmpty';
-export { isFunction } from './types/isFunction';
-export { isIterable } from './types/isIterable';
-export { isNode } from './types/isNode';
-export { isNull } from './types/isNull';
-export { isNullOrUndefined } from './types/isNullOrUndefined';
-export { isNumber } from './types/isNumber';
-export { isObject } from './types/isObject';
-export { isString } from './types/isString';
-export { isTextNode } from './types/isTextNode';
-export { isUndefined } from './types/isUndefined';
+export { default as areEqual } from './types/areEqual'
+export { isArray } from './types/isArray'
+export { isBoolean } from './types/isBoolean'
+export { isEmpty } from './types/isEmpty'
+export { isFunction } from './types/isFunction'
+export { isIterable } from './types/isIterable'
+export { isNode } from './types/isNode'
+export { isNull } from './types/isNull'
+export { isNullOrUndefined } from './types/isNullOrUndefined'
+export { isNumber } from './types/isNumber'
+export { isObject } from './types/isObject'
+export { isString } from './types/isString'
+export { isTextNode } from './types/isTextNode'
+export { isUndefined } from './types/isUndefined'
