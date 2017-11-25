f764891e1cf839f6496c723f58cbb47fd4e4e936
Forrest Collins <Forest.d.collins@gmail.com>
2017-11-25 00:01:47 -0600

    Changes the frost-eslint config a bit, making it more relaxed for the time being

--- packages/frost-eslint/src/index.js
+++ packages/frost-eslint/src/index.js
@@ -1,12 +1,49 @@
 module.exports = {
-  extends: [
-    './rules/base.yml',
-    './rules/import.yml',
-    './rules/security.yml',
-    './rules/modern.yml',
-    './rules/node.yml',
-    './rules/react.yml',
-    './rules/formatting.yml',
-    './rules/promise.yml',
-  ],
-};
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            generators: true,
+            experimentalObjectRestSpread: true
+        },
+        sourceType: 'module',
+        allowImportExportEverywhere: false
+    },
+    extends: ['airbnb'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.json', '.css']
+            }
+        }
+    },
+    globals: {
+        window: true,
+        document: true,
+        __dirname: true,
+        process: true,
+        test: true,
+        fetch: true
+    },
+    rules: {
+        'import/extensions': [
+            'error',
+            'always',
+            {
+                js: 'never',
+                jsx: 'never'
+            }
+        ],
+        'no-shadow': 0,
+        'no-use-before-define': 0,
+        'no-param-reassign': 0,
+        'no-confusing-arrow': 0,
+        'no-underscore-dnage': 0,
+        'no-plusplus': 0,
+        'prefer-template': 1,
+        'global-require': 1,
+        'import/no-named-default': 1,
+        'no-console': 1,
+        'consistent-return': 0,
+        'import/prefer-default-export': 1
+    }
+}
