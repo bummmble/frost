782c1e0db731af408cf1197a1e6b2b288a7b1c42
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-01 19:25:52 -0600

    lays out idea for builder class

---
+++ packages/frost-builder/src/core/builder.js
@@ -0,0 +1,56 @@
+import webpack from 'webpack';
+import { each } from 'frost-utils';
+
+export default class Builder {
+    constructor() {
+        this.compilers = [];
+    }
+
+    async build(compilers, env) {
+        await this.buildWebpack(compilers, env);
+        return this;
+    }
+
+    async buildWebpack(compilers, env) {
+        const cache = {};
+
+        this.compilers = compilers.map(config => {
+            const compiler = webpack(config);
+            compiler.cache = cache;
+            return compiler;
+        });
+
+        await each(this.compilers, compiler => {
+            return new Promise((resolve, reject) => {
+                compiler.plugin('done', stats => {
+                    process.nextTick(resolve);
+                });
+
+                compiler.run((err, stats) => {
+                    if (err) {
+                        console.error(err);
+                        return reject(err);
+                    }
+
+                    if (stats.hasErrors()) {
+                        return reject(new Error('Webpack build failed'));
+                    }
+                })
+            })
+        })
+
+        return this;
+    }
+
+    startDev(multiCompiler) {
+        return new Promise((resolve, reject) => {
+            multiCompiler.plugin('invalid', () => {
+                console.log('Dev Server Compiling');
+            });
+
+            multiCompiler.plugin('done', stats => {
+                return resolve(stats);
+            });
+        });
+    }
+}
