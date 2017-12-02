3ee540f842a8afb0e17e72483a629bb158278edf
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-01 20:01:46 -0600

    Adds some events to the builder

--- packages/frost-builder/src/core/builder.js
+++ packages/frost-builder/src/core/builder.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import { each } from 'frost-utils';
+import { emitEvent } from './emitter';
 
 export default class Builder {
     constructor() {
@@ -7,7 +8,12 @@ export default class Builder {
     }
 
     async build(compilers, env) {
+        emitEvent('before-webpack-build', compilers);
+
         await this.buildWebpack(compilers, env);
+
+        emitEvent('after-webpack-build');
+
         return this;
     }
 
@@ -21,8 +27,10 @@ export default class Builder {
         });
 
         await each(this.compilers, compiler => {
+            emitEvent(`before-webpack-${compiler.options.name}-compilation`);
             return new Promise((resolve, reject) => {
                 compiler.plugin('done', stats => {
+                    emitEvent('after-webpack-compilation', stats);
                     process.nextTick(resolve);
                 });
 
@@ -43,12 +51,14 @@ export default class Builder {
     }
 
     startDev(multiCompiler) {
+        emitEvent('before-dev-server-start', multiCompiler);
         return new Promise((resolve, reject) => {
             multiCompiler.plugin('invalid', () => {
                 console.log('Dev Server Compiling');
             });
 
             multiCompiler.plugin('done', stats => {
+                emitEvent('after-dev-server-build', stats);
                 return resolve(stats);
             });
         });
