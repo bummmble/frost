224561c196ee8b167dde3e16182200c02de2fbaa
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-02 20:05:24 -0600

    Removes unused style code from server compiler

--- packages/frost-builder/src/compilers/server.js
+++ packages/frost-builder/src/compilers/server.js
@@ -3,19 +3,11 @@ import BaseCompiler from './base';
 
 export default function ServerCompiler(props, config) {
     const base = BaseCompiler(props, config);
-    const { isDev, isProd } = props;
 
     base.name = 'server';
     base.target = 'node';
     base.entry.main = config.entry.server;
     base.output.path = config.output.server;
-
-    base.module.rules.push({
-        test: config.files.styles,
-        use: {
-            loader: 'css-loader/locals'
-        }
-    });
 
     base.plugins.push(new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
