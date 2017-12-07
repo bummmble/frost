fb63886dc3e48c7d90f10d396583549ca1ba182a
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-07 10:34:39 -0600

    Fixes some small spacing issues in frost-util readme

--- packages/frost-utils/src/promise/promiseEach.js
+++ packages/frost-utils/src/promise/promiseEach.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax no-await-in-loop import/prefer-default-export */
 export async function promiseEach(arr, fn) {
   for (const item of arr) {
     await fn(item);
