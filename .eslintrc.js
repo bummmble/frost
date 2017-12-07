55e061f59544068ca5bc6bd1bef44fb40eab0efc
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-06 23:38:48 -0600

    Updates linting deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
-
 module.exports = {
-    extends: ["frost"],
-    rules: {
-        'promise/params-names': 1,
-        'import/no-unresolved': 0,
-        'import/named': 0,
-        'import/default': 0,
-        'import/namespace': 0,
-        'import/no-commonjs': 0,
-        'no-console': 0
-    }
-}
+  extends: ['frost'],
+  rules: {
+    'promise/params-names': 1,
+    'import/no-unresolved': 0,
+    'import/named': 0,
+    'import/default': 0,
+    'import/namespace': 0,
+    'import/no-commonjs': 0
+  }
+};
