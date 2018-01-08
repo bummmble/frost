d496321e5a1ebcca4ddd72bf26271c051b5af2b6
Forrest Collins <Forest.d.collins@gmail.com>
2018-01-07 18:33:39 -0600

    add tests for the destination helpers with 100% coverage

--- tests/unit/compilers/utilties.test.js
+++ tests/unit/compilers/utilties.test.js
@@ -93,3 +93,47 @@ test('If typesript is enabled then devtool should be changed', t => {
     const tool = configureDevtool(false, config);
     t.true(tool == 'inline-source-map');
 });
+
+// --- Destinations ---
+// configureEntry(isDev, isServer, config)
+// configureOutput(isDev, isServer, config)
+
+test('Should return the proper entry for server and client', t => {
+    const client = configureEntry(false, false, testConfig);
+    const server = configureEntry(false, true, testConfig);
+    t.true(client.main.includes(testConfig.entry.client));
+    t.true(server.main.includes(testConfig.entry.server));
+});
+
+test('Should add hmr and react-hot-loader to entry', t => {
+    const config = {
+        ...testConfig,
+        webpack: {
+            ...testConfig.webpack,
+            useHmr: true
+        },
+        framework: {
+            ...testConfig.framework,
+            name: 'react',
+        }
+    };
+    const entry = configureEntry(true, false, config);
+    t.true(entry.main.includes('react-hot-loader'));
+    t.true(entry.main.includes('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo-false'));
+});
+
+test('Should configure proper output for dev and server', t => {
+    const output = configureOutput(true, true, testConfig);
+    t.true(output.libraryTarget == 'commonjs2');
+    t.true(output.filename == '[name].js');
+    t.true(output.chunkFilename == '[name].js');
+    t.true(output.pathinfo === true);
+    t.true(output.path == testConfig.output.server);
+});
+
+test('Should handle production options for output', t => {
+    const output = configureOutput(false, false, testConfig);
+    t.true(output.filename == '[name].[chunkhash].js');
+    t.true(output.chunkFilename == '[name].[chunkhash].js');
+});
+
