b3e795b6356ea23f0d5a601cc10b5b624e1b4bf6
Forrest Collins <Forest.d.collins@gmail.com>
2018-01-07 19:30:20 -0600

    Integrates happy pack into the base configuration to generate all of the loaders and plugins

--- src/compilers/base.js
+++ src/compilers/base.js
@@ -10,6 +10,7 @@ import {
     configureLoaders
 } from './helpers';
 import { createHappyPlugins } from './plugins';
+import { filterOutKeys } from '../utils';
 
 export default function BaseCompiler(props, config) {
     const { isProd, isDev, isServer, isClient } = props;
@@ -21,11 +22,58 @@ export default function BaseCompiler(props, config) {
     const performance = configurePerformance(isDev, isServer, config);
     const loaders = configureLoaders(isDev, isServer, babelEnv, config);
 
+    const useHappyPack = config.webpack.useHappyPack;
+    const HappyPlugin = useHappyPack
+        ? config.styles.extract !== 'none' && !isServer
+            ? createHappyPlugins(filterOutKeys(loaders, ['styles']))
+            : createHappyPlugins(loaders)
+        : [];
+
     return {
         context: config.root,
         devtool,
         performance,
         entry: configureEntry(isDev, isServer, config),
-        output: configureOutput(isDev, isServer, config)
+        output: configureOutput(isDev, isServer, config),
+        module: {
+            rules: [
+                {
+                    test: config.files.babel,
+                    exclude: /node_modules/,
+                    use: useHappyPack
+                        ? 'happypack/loader?id=js'
+                        : loaders.js
+                },
+                {
+                    test: config.files.styles,
+                    exclude: /node_modules/,
+                    use: useHappyPack && (isServer || config.styles.extract === 'none')
+                        ? 'happypack/loader?id=styles'
+                        : loaders.style
+                },
+                config.useElm ? {
+                    test: config.files.elm,
+                    use: useHappyPack
+                        ? 'happypack/loader?id=elm'
+                        : loaders.elm
+                } : false,
+                config.useTypescript ? {
+                    test: config.files.typescript,
+                    use: useHappyPack
+                        ? 'happypack/loader?id=ts'
+                        : loaders.ts
+                } : false,
+                config.useRust ? {
+                    test: config.files.rust,
+                    use: useHappyPack
+                        ? 'happypack/loader?id=rust'
+                        : loaders.rust
+                }
+            ].filter(Boolean)
+        },
+
+        plugins: [
+            ...HappyPlugin
+        ]
     };
 }
